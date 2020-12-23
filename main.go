d32b46e4ec587e30d5ca0af1c71c32d9b80478a9
franciscocid <franciscocidadehomemjunior@gmail.com>
2020-12-23 20:38:47 -0300

    feat: random seed

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
+	"math/rand"
+	"time"
 
 	"github.com/franciscocid/vm/chip8"
 )
 
 func main() {
 
-	// g := chip8.NewAsciiGraphics(64, 32, "X", " ")
+	log.SetOutput(ioutil.Discard)
+
+	rand.Seed(time.Now().UnixNano())
+
 	program, err := ioutil.ReadFile("./programs/random_number_test.ch8")
 	if err != nil {
 		panic(err)
 	}
 
-	c8 := chip8.New()
+	g := chip8.NewAsciiMonitor(64, 32, "X", " ")
+	c8 := chip8.New(g)
 	c8.LoadProgram(program)
 	c8.Run()
 }
