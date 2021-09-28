a8e106505db403bd48d7138fdd781bf58248f973
fcidade <franciscocidadehomemjunior@gmail.com>
2021-09-28 17:38:05 -0300

    Font

--- chip8/graphics.go
+++ chip8/graphics.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/ttf"
 )
 
 var Keyboard2Chip8 = map[sdl.Keycode]uint8{
@@ -22,11 +23,13 @@ type SDLGraphics struct {
 	renderer *sdl.Renderer
 
 	c8 *Chip8
+
+	font *ttf.Font
 }
 
 func (g *SDLGraphics) Run() error {
-	err := g.setup()
-	if err != nil {
+
+	if err := g.setup(); err != nil {
 		return err
 	}
 
@@ -59,17 +62,98 @@ func (g *SDLGraphics) Run() error {
 		g.renderer.SetDrawColor(0, 0, 0, 0)
 		g.renderer.Clear()
 
-		g.renderer.SetDrawColor(255, 255, 255, 255)
+		pivotX, pivotY, pivotW, pivotH := 50, 50, ScreenWidth*4, ScreenHeight*4
+		borderSize := 10
 
-		for y := uint8(0); y < ScreenHeight; y++ {
-			for x := uint8(0); x < ScreenWidth; x++ {
-				if g.c8.CurrState.GetPixel(x, y) {
-					g.renderer.DrawPoint(int32(x), int32(y))
-				}
-			}
+		g.drawBackground(pivotX, pivotY, pivotW, pivotH, borderSize)
+		g.drawChip8(pivotX, pivotY, pivotW, pivotH)
+
+		err := g.text("teste", 100, 100)
+		if err != nil {
+			return err
 		}
 
 		g.renderer.Present()
+	}
+
+	return nil
+}
+
+func (g *SDLGraphics) text(msg string, x, y int) error {
+	t, err := g.font.RenderUTF8Blended(msg, sdl.Color{
+		R: 255,
+		G: 255,
+		B: 255,
+		A: 255,
+	})
+	if err != nil {
+		return err
+	}
+
+	texture, err := g.renderer.CreateTextureFromSurface(t)
+	if err != nil {
+		return err
+	}
+
+	err = g.renderer.Copy(texture, nil,
+
+		&sdl.Rect{
+			X: int32(x),
+			Y: int32(y),
+			W: 50,
+			H: 50,
+		})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *SDLGraphics) selectMainPalette() {
+	g.renderer.SetDrawColor(194, 62, 128, 255)
+}
+
+func (g *SDLGraphics) selectBackgroundPalette() {
+	g.renderer.SetDrawColor(0, 0, 0, 0)
+}
+
+func (g *SDLGraphics) drawBackground(pivotX, pivotY, pivotW, pivotH, borderSize int) {
+	backgroundRect := &sdl.Rect{
+		X: int32(pivotX - borderSize),
+		Y: int32(pivotY - borderSize),
+		W: int32(pivotW + borderSize*2),
+		H: int32(pivotH + borderSize*2),
+	}
+	g.selectMainPalette()
+	g.renderer.FillRect(backgroundRect)
+	foregroundRect := &sdl.Rect{
+		X: int32(pivotX),
+		Y: int32(pivotY),
+		W: int32(pivotW),
+		H: int32(pivotH),
+	}
+	g.selectBackgroundPalette()
+	g.renderer.FillRect(foregroundRect)
+}
+
+func (g *SDLGraphics) drawChip8(pivotX, pivotY, pivotW, pivotH int) error {
+	g.selectMainPalette()
+	pixelWidth := int(pivotW / ScreenWidth)
+	pixelHeight := int(pivotH / ScreenHeight)
+
+	for y := 0; y < ScreenHeight; y++ {
+		for x := 0; x < ScreenWidth; x++ {
+			if g.c8.CurrState.GetPixel(uint8(x), uint8(y)) {
+				rect := &sdl.Rect{
+					X: int32(pivotX + x*pixelWidth),
+					Y: int32(pivotY + y*pixelHeight),
+					W: int32(pixelWidth),
+					H: int32(pixelHeight),
+				}
+				g.renderer.FillRect(rect)
+			}
+		}
 	}
 
 	return nil
@@ -79,6 +163,16 @@ func (g *SDLGraphics) setup() error {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		return err
 	}
+
+	if err := ttf.Init(); err != nil {
+		return err
+	}
+
+	font, err := ttf.OpenFont("./assets/monogram.ttf", 12) // TODO: Make it dynamic
+	if err != nil {
+		return err
+	}
+	g.font = font
 
 	window, err := sdl.CreateWindow(
 		g.Title,
@@ -97,7 +191,7 @@ func (g *SDLGraphics) setup() error {
 		return err
 	}
 
-	renderer.SetLogicalSize(int32(ScreenWidth), int32(ScreenHeight))
+	// renderer.SetLogicalSize(int32(ScreenWidth), int32(ScreenHeight))
 	g.renderer = renderer
 	return nil
 }
@@ -122,8 +216,8 @@ func (g *SDLGraphics) handleEvents() {
 func NewGraphicsSDL(c8 *Chip8) *SDLGraphics {
 	return &SDLGraphics{
 		Title:   "Chip-8",
-		Width:   ScreenWidth * 8,
-		Height:  ScreenHeight * 8,
+		Width:   600,
+		Height:  400,
 		running: true,
 		c8:      c8,
 	}
